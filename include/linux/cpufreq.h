ea934400b497724ad967aff3eff097750079c47e
charles1018 <chmadux8@gmail.com>
2015-04-23 11:10:29 +0800

    added cpufreq governor intellidemand

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -390,6 +390,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intellidemand)
 
 #endif
 
